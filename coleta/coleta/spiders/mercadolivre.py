3dafd983763378042524c9ac5253145a161f5b1c
Rafael Simoes <simoesrhf@gmail.com>
2025-02-03 21:34:34 -0300

    second commit

---
+++ coleta/coleta/spiders/mercadolivre.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class MercadolivreSpider(scrapy.Spider):
+    name = "mercadolivre"
+    allowed_domains = ["lista.mercadolivre.com.br"]
+    start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino#D%5BA:tenis%20corrida%20masculino%5D"]
+
+    def parse(self, response):
+        pass
